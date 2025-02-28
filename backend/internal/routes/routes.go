0cec3d19dbfa034e40d1549e82704a30581f8ff8
LostProgrammer1010 <dmeyer20@URMC-SH.Rochester.edu>
2025-02-27 22:57:46 -0500

    Adding notes on routes that will be needed

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -5,6 +5,25 @@ import (
 	"net/http"
 )
 
+/*
+Routes that will be need
+
+Users:
+GET users/search/{username} - Finds all users that match search
+GET user/{username} - pulls all information about a user
+POST user/group/add/{username} - adds user to group
+POST user/update/{username} - update user information
+
+Groups
+GET groups/search/{Group} - Find all Groups that match search
+GET group/members/{Group} - pulls all the memebers of the ad group
+GET group/{Group} - Pulls information about the AD Group
+
+Computer
+GET computers/search/{Group} - Find all computers that match Search
+
+*/
+
 func NewRouter() http.Handler {
 	mux := http.NewServeMux()
 
@@ -12,82 +31,3 @@ func NewRouter() http.Handler {
 
 	return mux
 }
-
-/*
-// Handler for POST request to receive messages
-func handlePost(w http.ResponseWriter, r *http.Request) {
-
-	option.EnableCORS(w, r)
-
-	fmt.Println(r.URL.Path)
-	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
-	}
-	// Check if the request method is POST
-	// Read the body of the request
-	var input Input
-
-	fmt.Println(r.Body)
-	err := json.NewDecoder(r.Body).Decode(&input)
-	if err != nil {
-		http.Error(w, "Failed to parse JSON", http.StatusBadRequest)
-		return
-	}
-
-	// Log the received message
-	fmt.Printf("Searching for:  %s\n", input.Value)
-
-	if input.Value == "" {
-		return
-	}
-
-	matches := findUser(input.Value)
-
-	// Set the response header to application/json
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK) // Send 200 OK status
-
-	jsonData, _ := json.Marshal(matches)
-
-	// Write the response to the client
-	w.Write(jsonData)
-}
-
-func findUser(input string) (matches []string) {
-
-	fmt.Println(input)
-
-	l, err := server.ConnectToServer("LDAP://urmc-sh.rochester.edu/")
-	fmt.Println(err)
-	defer l.Close()
-
-	searchRequest := ldap.NewSearchRequest(
-		"DC=urmc-sh,DC=rochester,DC=edu",
-		ldap.ScopeWholeSubtree,
-		ldap.NeverDerefAliases,
-		0, // Max search size 1500
-		0, // No timeout for search
-		false,
-		fmt.Sprintf("(&(objectClass=user)(|(SAMAccountName=%s*)(givenName=%s*)(sn=%s*)))", input, input, input), //Filter
-		[]string{"cn", "distinguishedName", "name", "sAMAccountName"},                                           // Attributes
-		nil,
-	)
-
-	results, _ := l.Search(searchRequest)
-
-	for _, entry := range results.Entries {
-
-		matches = append(matches, fmt.Sprintf("%s | %s | %s", entry.GetAttributeValue("sAMAccountName"), entry.GetAttributeValue("name"), entry.GetAttributeValue("distinguishedName")))
-	}
-
-	err = l.Unbind()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return
-
-}
-
-*/
