9ecdc88994a3eb70b59382b49b2af1fd39b48225
LostProgrammer1010 <dmeyer20@URMC-SH.Rochester.edu>
2025-02-27 22:57:46 -0500

    Adding notes on routes that will be needed

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -8,6 +8,7 @@ import (
 /*
 Routes that will be need
 
+<<<<<<< HEAD
 All:
 GET all/search/{input}
 
@@ -22,6 +23,18 @@ Groups
 GET groups/search/{input} - Find all Groups that match search
 GET group/members/{input} - pulls all the memebers of the ad group
 GET group/{input} - Pulls information about the AD Group
+=======
+Users:
+GET users/search/{username} - Finds all users that match search
+GET user/{username} - pulls all information about a user
+POST user/group/add/{username} - adds user to group
+POST user/update/{username} - update user information
+
+Groups
+GET groups/search/{Group} - Find all Groups that match search
+GET group/members/{Group} - pulls all the memebers of the ad group
+GET group/{Group} - Pulls information about the AD Group
+>>>>>>> 0cec3d1 (Adding notes on routes that will be needed)
 
 Computer
 GET computers/search/{Group} - Find all computers that match Search
