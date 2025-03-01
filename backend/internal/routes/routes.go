57c1f1785a1e28613c0c95bf76d8d900ac8438e2
LostProgrammer1010 <dmeyer20@URMC-SH.Rochester.edu>
2025-02-28 23:55:48 -0500

    Printer Queue and Groups backend

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -12,15 +12,16 @@ All:
 GET all/search/{input}
 
 Users:
-GET users/search/{username} - Finds all users that match search
-GET user/{username} - pulls all information about a user
-POST user/group/add/{username} - adds user to group
-POST user/update/{username} - update user information
+GET users/search/{input} - Finds all users that match search
+GET users/search/UR/{input} - Finds all users that match search with in UR directory
+GET user/{input} - pulls all information about a user
+POST user/group/add/{input} - adds user to group
+POST user/update/{input} - update user information
 
 Groups
-GET groups/search/{Group} - Find all Groups that match search
-GET group/members/{Group} - pulls all the memebers of the ad group
-GET group/{Group} - Pulls information about the AD Group
+GET groups/search/{input} - Find all Groups that match search
+GET group/members/{input} - pulls all the memebers of the ad group
+GET group/{input} - Pulls information about the AD Group
 
 Computer
 GET computers/search/{Group} - Find all computers that match Search
@@ -30,6 +31,7 @@ GET computers/search/{Group} - Find all computers that match Search
 func NewRouter() http.Handler {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("/groups/search/", get.GroupsSearch)
 	mux.HandleFunc("/users/search/", get.UsersSearch)
 
 	return mux
