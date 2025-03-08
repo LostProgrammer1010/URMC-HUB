ee8eb3c528391ff817044d6cade11ea7123b7bcc
LostProgrammer1010 <04_zoster_borscht@icloud.com>
2025-03-08 12:29:41 -0500

    Update branch to master

--- backend/cmd/hub/main.go
+++ backend/cmd/hub/main.go
@@ -27,3 +27,49 @@ func main() {
 	//post.FindShareDrive("NTSDRIVE05")
 
 }
+
+/*
+func makereq() {
+
+	// Define your username and password
+	username := "dmeyer20"
+	password := "PoleVault10"
+
+	// Combine username and password and encode them in Base64
+	auth := username + ":" + password
+	encodedAuth := base64.StdEncoding.EncodeToString([]byte(auth))
+
+	// Define the URL
+	url := "https://myidentity.rochester.edu/identity/admin/"
+
+	// Create a new HTTP request with the GET method
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	// Set the Authorization header with the encoded credentials
+	req.Header.Set("Authorization", "Basic "+encodedAuth)
+
+	// Create an HTTP client and execute the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Read and print the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+
+	// Print the response status and body
+	fmt.Println("Response Status:", resp.Status)
+	fmt.Println("Response Body:", string(body))
+}
+*/
