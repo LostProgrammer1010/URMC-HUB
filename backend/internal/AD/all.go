d7bb2489e59340cb160a3dd121ebc56bf40ff8e7
jpatrick5402 <jpatrick5402@gmail.com>
2025-03-13 14:59:05 -0400

    Adding shares and printers to all search

--- backend/internal/AD/all.go
+++ backend/internal/AD/all.go
@@ -1,9 +1,15 @@
 package AD
 
+import (
+	"backend/internal/api/post"
+)
+
 type AllResult struct {
-	Users     []User           `json:"users"`
-	Computers []ComputerResult `json:"computers"`
-	Groups    []string         `json:"groups"`
+	Users     []User            `json:"users"`
+	Computers []ComputerResult  `json:"computers"`
+	Groups    []string          `json:"groups"`
+	Printers  []post.Printer    `json:"printers"`
+	Shares    []post.ShareDrive `json:"shares"`
 }
 
 func AllSearch(search string, domain string) (result AllResult) {
@@ -13,6 +19,10 @@ func AllSearch(search string, domain string) (result AllResult) {
 	result.Computers = ComputersSearch(search)
 
 	result.Groups = GroupsSearch(search)
+
+	result.Printers = post.MatchPrinter(search)
+
+	result.Shares = post.FindShareDrive(search)
 
 	return
 }
