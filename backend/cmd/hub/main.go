dbacc15f171e34b2de5504072a7adc6b058dfa92
LostProgrammer1010 <04_zoster_borscht@icloud.com>
2025-03-20 13:18:18 -0400

    beta tester branch

--- backend/cmd/hub/main.go
+++ backend/cmd/hub/main.go
@@ -3,31 +3,18 @@ package main
 import (
 	"backend/internal/AD"
 	"backend/internal/server"
-	"fmt"
-	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
 
-	err := godotenv.Load()
+	//err := godotenv.Load()
 
-	fmt.Println(err)
+	//fmt.Println(err)
 
-	AD.Username = os.Getenv("username")
-	AD.Password = os.Getenv("password")
+	//AD.Username = os.Getenv("username")
+	//AD.Password = os.Getenv("password")
 
-
-
-
-
-
-
-	//AD.GroupsSearch("ISDG_CTX_eRecord2")
-
-
-	//AD.Login()
+	AD.Login()
 	//AddToStartup() //once application is finished this can be turn on to put application in startup folder
 	server.Start()
 
