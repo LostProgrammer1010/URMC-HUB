1ef74b729878b2640102fb8e14a0b21d99e138fc
LostProgrammer1010 <04_zoster_borscht@icloud.com>
2025-03-20 23:07:49 -0400

    #82 Go routes implemented

--- backend/internal/all/all.go
+++ backend/internal/all/all.go
@@ -1,6 +1,11 @@
 package all
 
-import "backend/internal/AD"
+import (
+	"backend/internal/AD"
+	"fmt"
+	"sync"
+	"time"
+)
 
 type AllResult struct {
 	Users     []AD.User       `json:"users"`
@@ -12,15 +17,45 @@ type AllResult struct {
 
 func AllSearch(search string, domain string) (result AllResult) {
 
-	result.Users = AD.UsersSearch(search, domain)
+	fmt.Println(time.Now())
 
-	result.Computers = AD.ComputersSearch(search)
+	var wg sync.WaitGroup
+	ch := make(chan interface{}, 5)
 
-	result.Groups = AD.GroupsSearch(search)
+	wg.Add(5)
+	go thread(&wg, ch, func() interface{} { return AD.ComputersSearch(search) })
+	go thread(&wg, ch, func() interface{} { return AD.UsersSearch(search, domain) })
+	go thread(&wg, ch, func() interface{} { return AD.GroupsSearch(search) })
+	go thread(&wg, ch, func() interface{} { return AD.MatchPrinter(search) })
+	go thread(&wg, ch, func() interface{} { return AD.FindShareDrive(search) })
 
-	result.Printers = AD.MatchPrinter(search)
+	wg.Wait()
+	close(ch)
 
-	result.Shares = AD.FindShareDrive(search)
+	for results := range ch {
+		switch results := results.(type) {
+		case []AD.ShareDrive:
+			result.Shares = results
+		case []AD.Computer:
+			result.Computers = results
+		case []AD.User:
+			result.Users = results
+		case []AD.Group:
+			result.Groups = results
+		case []AD.Printer:
+			result.Printers = results
+		default:
+			fmt.Println(results)
+		}
+
+	}
+
+	fmt.Println(time.Now())
 
 	return
+}
+
+func thread(wg *sync.WaitGroup, ch chan any, task func() any) {
+	defer wg.Done()
+	ch <- task()
 }
